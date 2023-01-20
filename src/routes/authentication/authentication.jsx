440fb5636e7d928ec7760c1012b8a0070b3a27f7
Andrey <andrey.timakhovich@rubyroidlabs.com>
2023-01-20 15:31:02 +0300

    forbid access to /auth when a user is already logged in

--- src/routes/authentication/authentication.jsx
+++ src/routes/authentication/authentication.jsx
@@ -1,14 +1,41 @@
+import { useEffect, Fragment } from "react"
+import { useNavigate } from "react-router-dom"
+import { useSelector, useDispatch } from "react-redux"
 import SignInForm from "../../components/sign-in-form/sign-in-form"
 import SignUpForm from "../../components/sign-up-form/sign-up-form"
+import { CircularProgress } from "@mui/material"
 
-import { AuthenticationContainer } from "./authentication.styles"
+import { selectCurrentUser, selectCurrentUserIsLoading } from "../../store/user/user-selector"
+import { showFlashMessageAsync } from "../../store/flash/flash-action"
+
+import { AuthenticationContainer, Loader } from "./authentication.styles"
 
 const Authentication = () => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const currentUser = useSelector(selectCurrentUser)
+  const currentUserIsLoading = useSelector(selectCurrentUserIsLoading)
+
+  useEffect(() => {
+    if (currentUser) {
+      navigate("/")
+      dispatch(showFlashMessageAsync("You're already signed in"))
+    }
+  }, [currentUserIsLoading])
+
   return (
-    <AuthenticationContainer>
-      <SignInForm />
-      <SignUpForm />
-    </AuthenticationContainer>
+    <Fragment>
+      {currentUserIsLoading ? (
+        <Loader>
+          <CircularProgress color="inherit" />
+        </Loader>
+      ) : (
+        <AuthenticationContainer>
+          <SignInForm />
+          <SignUpForm />
+        </AuthenticationContainer>
+      )}
+    </Fragment>
   )
 }
 
